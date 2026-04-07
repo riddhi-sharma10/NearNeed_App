3361fd14038c24617e8e129a1350233f2d256dd5
Reia <reiabaid330@gmail.com>
2026-04-07 20:45:09 +0530

    changed naming conventions and removed unnecessary files

--- app/src/main/java/com/example/nearneed/WelcomeActivity.java
+++ app/src/main/java/com/example/nearneed/WelcomeActivity.java
@@ -16,7 +16,7 @@ public class WelcomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.welcome_activity);
+        setContentView(R.layout.activity_welcome);
 
         setupSlideshow();
 

9a304db05a90d5c3bd0bb4a5a0748b27eadea46e
riddhi-sharma10 <riddhisharmawave18@gmail.com>
2026-04-07 19:28:40 +0530

    Established connection

--- app/src/main/java/com/example/nearneed/ProfileSetupActivity.java
+++ app/src/main/java/com/example/nearneed/ProfileSetupActivity.java
@@ -45,7 +45,7 @@ public class ProfileSetupActivity extends AppCompatActivity {
         btnBack.setOnClickListener(v -> onBackPressed());
 
         btnContinue.setOnClickListener(v -> {
-            Intent intent = new Intent(this, AccountTypeActivity.class);
+            Intent intent = new Intent(this, ProfileSuccessActivity.class);
             startActivity(intent);
             overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
         });

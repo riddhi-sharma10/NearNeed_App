4493258c671751a870af08cb8c2cd73d4a010610
riddhi-sharma10 <riddhisharmawave18@gmail.com>
2026-04-11 22:36:19 +0530

    necessary drawables

---
+++ app/src/main/java/com/example/nearneed/HomeProviderActivity.java
@@ -0,0 +1,15 @@
+package com.example.nearneed;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeProviderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_provider);
+        
+        // Navigation and status logic could be added here
+    }
+}
